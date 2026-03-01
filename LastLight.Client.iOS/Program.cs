c04c91a20e274b85a197781755e5dc8f6ebe68e5
sean diamond <seanwdiamond@gmail.com>
2026-03-01 16:40:16 -0600

    added ios project

---
+++ LastLight.Client.iOS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using LastLight.Client.Core;
+
+namespace LastLight.Client.iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static LastLight.Client.Core.Game1 game;
+
+        internal static void RunGame()
+        {
+            game = new LastLight.Client.Core.Game1();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
